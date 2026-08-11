3c84e76d06aad3ce9910baf9bd04a066baa7a6a6
Shahid Khan <84471562+mdshahidalikhan02@users.noreply.github.com>
2026-08-11 14:24:14 +0530

    Update content.ts

--- src/data/content.ts
+++ src/data/content.ts
@@ -9,7 +9,7 @@ export const profile = {
   email: "2000shahid2019@gmail.com",
   linkedin: "https://linkedin.com/in/md-shahid-ali-khan-aa18371b0",
   linkedinLabel: "linkedin.com/mdshahidalikhan",
-  github: "https://github.com/mdshahidalikhan",
+  github: "https://github.com/mdshahidalikhan02",
   githubLabel: "github.com/mdshahidalikhan",
   location: "Bangalore, India",
   summary:
